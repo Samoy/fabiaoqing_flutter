c3309be0060cf1918a38b16bdea8416d47897dbc
Samoy <samoy_young@163.com>
2019-12-21 15:03:48 +0800

    表情包搜索页优化

--- lib/delegate/search_delagate.dart
+++ lib/delegate/search_delagate.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 import 'dart:io';
 import '../widgets/packages_widget.dart';
 
-/// 搜索功能以下未实现
-/// 1.与应用主题不匹配
-/// 2.占位符应该使用中文
-/// 3.历史记录
+//TODO:搜索未实现历史记录功能
 class MySearchDelegate extends SearchDelegate {
   @override
   String get searchFieldLabel => "请输入搜索内容";
@@ -37,6 +35,14 @@ class MySearchDelegate extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     return new PackagesWidget(keyword: query);
+  }
+
+  @override
+  void showResults(BuildContext context) {
+    super.showResults(context);
+    if (query.isEmpty) {
+      Toast.show("请先输入搜索内容哦，^_^", context, gravity: Toast.CENTER);
+    }
   }
 
   @override
